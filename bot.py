e6e11dafd38e098a665feadfef698330f6ddcb9e
Artem Yevtieiev <artem.yevteev@gmail.com>
2026-02-13 14:00:17 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,38 @@
+from telebot import TeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from logic import *
+import schedule
+import threading
+import time
+from config import *
+
+bot = TeleBot(API_TOKEN)
+
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_id = message.chat.id
+    if user_id in manager.get_users():
+        bot.reply_to(message, "Ты уже зарегестрирован!")
+    else:
+        manager.add_user(user_id, message.from_user.username)
+        bot.reply_to(message, """Привет! Добро пожаловать! 
+Тебя успешно зарегистрировали!
+Каждый час тебе будут приходить новые картинки и у тебя будет шанс их получить!
+Для этого нужно быстрее всех нажать на кнопку 'Получить!'
+
+Только три первых пользователя получат картинку!)""")
+        
+
+
+def polling_thread():
+    bot.polling(none_stop=True)
+
+if __name__ == '__main__':
+    manager = DatabaseManager(DATABASE)
+    manager.create_tables()
+    polling_thread = threading.Thread(target=polling_thread)
+    polling_schedule = threading.Thread(target=schedule_thread)
+    polling_sсhedule.start()
+  
